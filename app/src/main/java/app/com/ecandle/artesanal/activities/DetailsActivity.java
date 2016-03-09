9e7ba5daf4f10c67c821f1c54b9ee1f54d275b27
juantomaylla <juan.tomaylla@gmail.com>
2016-03-09 17:33:55 -0500

    first commit

---
+++ app/src/main/java/app/com/ecandle/artesanal/activities/DetailsActivity.java
@@ -0,0 +1,50 @@
+package app.com.ecandle.artesanal.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import app.com.ecandle.artesanal.R;
+import app.com.ecandle.artesanal.models.Contact;
+public class DetailsActivity extends AppCompatActivity {
+    public static final String EXTRA_CONTACT = "EXTRA_CONTACT";
+    private Contact mContact;
+    private ImageView ivProfile;
+    private TextView tvName;
+    private TextView tvPhone;
+    private View vPalette;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        Bundle data = getIntent().getExtras();
+
+        ivProfile = (ImageView) findViewById(R.id.ivProfile);
+        tvName = (TextView) findViewById(R.id.tvName);
+        tvPhone = (TextView) findViewById(R.id.tvPhone);
+        vPalette = findViewById(R.id.vPalette);
+//test
+        // Extract contact from bundle
+        mContact = (Contact)getIntent().getExtras().getSerializable(EXTRA_CONTACT);
+
+        // Fill views with data
+        if (!data.getString("cName").isEmpty() && !data.getString("cNumber").isEmpty()  ){
+
+            Picasso.with(DetailsActivity.this).load(data.getInt("cImageId")).fit().centerCrop().into(ivProfile);
+            tvName.setText(data.getString("cName"));
+            tvPhone.setText(data.getString("cNumber"));
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+}
---
+++ app/src/main/java/app/com/ecandle/artesanal/adapters/ContactsAdapter.java
@@ -0,0 +1,101 @@
+package app.com.ecandle.artesanal.adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import app.com.ecandle.artesanal.R;
+import app.com.ecandle.artesanal.activities.DetailsActivity;
+import app.com.ecandle.artesanal.models.Contact;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+// Artesanal Jtomaylla 2016-03-09
+// Provide the underlying view for an individual list item.
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.VH> {
+    private Activity mContext;
+    private List<Contact> mContacts;
+
+    public ContactsAdapter(Activity context, List<Contact> contacts) {
+        mContext = context;
+        if (contacts == null) {
+            throw new IllegalArgumentException("contacts must not be null");
+        }
+        mContacts = contacts;
+    }
+
+    // Inflate the view based on the viewType provided.
+    @Override
+    public VH onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_contact, parent, false);
+        return new VH(itemView, mContext);
+    }
+
+    // Display data at the specified position
+    @Override
+    public void onBindViewHolder(VH holder, int position) {
+        Contact contact = mContacts.get(position);
+        holder.rootView.setTag(contact);
+        holder.tvName.setText(contact.getName());
+        Picasso.with(mContext).load(contact.getThumbnailDrawable()).fit().centerCrop().into(holder.ivProfile);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mContacts.size();
+    }
+
+    // Provide a reference to the views for each contact item
+    public final static class VH extends RecyclerView.ViewHolder {
+        final View rootView;
+        final ImageView ivProfile;
+        final TextView tvName;
+        final View vPalette;
+
+        public VH(View itemView, final Context context) {
+            super(itemView);
+            rootView = itemView;
+            ivProfile = (ImageView)itemView.findViewById(R.id.ivProfile);
+            tvName = (TextView)itemView.findViewById(R.id.tvName);
+            vPalette = itemView.findViewById(R.id.vPalette);
+
+            // Navigate to contact details activity on click of card view.
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    final Contact contact = (Contact)v.getTag();
+                    if (contact != null) {
+                        // Fire an intent when a contact is selected
+                        // Pass contact object in the bundle and populate details activity.
+                        // first parameter is the context, second is the class of the activity to launch
+
+                        // brings up the second activity
+                        Log.i("VH_","Clicked");
+                        Bundle extras = new Bundle();
+                        Intent i = new Intent(v.getContext(),DetailsActivity.class);
+                        // put "extras" into the bundle for access in the second activity
+                        extras.putString("cName", contact.getName());
+                        extras.putString("cNumber", contact.getNumber());
+                        extras.putInt("cImageId", contact.getThumbnailDrawable());
+                        i.putExtras(extras);
+                        context.startActivity(i);
+                    }
+                }
+            });
+        }
+    }
+
+//
+//    public void showDetails(Contact contact,Context context){
+//
+//
+//    }
+}
